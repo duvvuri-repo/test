c7464e78ec4ed518835e901d8c9c3ff418fd69ac
Sarvani <duvvvuris95@gmail.com>
2024-04-26 15:59:19 +0530

    Revert "Added burger,milk"
    This reverts commit adab2fded0be5f678939939a89678b3567023748.

--- hungry.py
+++ hungry.py
@@ -3,6 +3,6 @@ if hungry == "yes":
     print("eat samosas")
     print ("have some samosas")
     print("eat burger")
-    print("have some milkyrgit ")
+    print("have some milkyr")
 else:
     print("do your homework!")
